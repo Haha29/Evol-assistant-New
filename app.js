22f97306c586e7775358f8ccea0b4f4c4f1d80b6
Haha29 <85965127+Haha29@users.noreply.github.com>
2025-07-10 10:57:39 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+
+document.getElementById('root').innerHTML = `
+  <h1 style="text-align:center">EVOL 100 Breakdown Assistant</h1>
+  <p style="text-align:center">Use this tool to diagnose common machine problems quickly.</p>
+`;
